875bee72cef186f8c3877888a95d1ee08dd7f3c3
Gaurav Kapatia <gkapatia@gmail.com>
2024-04-24 14:57:06 +0530

    updating solution repo

--- src/main/java/com/newtonschool/question/Application.java
+++ src/main/java/com/newtonschool/question/Application.java
@@ -1,16 +1,16 @@
-package FileHandling;
+package com.newtonschool.question;
 
 import java.io.InputStream;
 import java.util.Scanner;
 
-public class FileReaderProject {
+public class Application {
     public static void main(String[] args) {
         readFromFile("dummy.txt");
     }
 
     public static void readFromFile(String fileName) {
         // Load file from resources folder using classloader
-        InputStream inputStream = FileReaderProject.class.getClassLoader().getResourceAsStream(fileName);
+        InputStream inputStream = Application.class.getClassLoader().getResourceAsStream(fileName);
         if (inputStream == null) {
             System.out.println("File not found: " + fileName);
             return;
